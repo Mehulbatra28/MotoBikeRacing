5d4a243786b98e0a49b27bbd6a22b85e36abb87d
Mehul Batra <mehulbatra28@gmail.com>
2025-08-25 23:22:25 +0530

    bike controller added

---
+++ bike game/Assets/Scripts/BikeController.cs
@@ -0,0 +1,25 @@
+using UnityEngine;
+
+public class BikeController : MonoBehaviour
+{
+    public Rigidbody2D frontTire;
+    public Rigidbody2D backTire;
+    public Rigidbody2D Bike;
+    public float speed = 10000f;
+    public float rotationSpeed = 300f;
+
+    private float moveInput;
+
+   public void Update()
+    {
+        moveInput = Input.GetAxisRaw("Horizontal");
+
+    }
+    private void FixedUpdate()
+    {
+        frontTire.AddTorque(moveInput * speed * Time.fixedDeltaTime);
+        backTire.AddTorque(moveInput * speed * Time.fixedDeltaTime);
+        Bike.AddTorque(moveInput * speed * Time.fixedDeltaTime);
+    }
+    
+}
